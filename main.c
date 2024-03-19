e55c6c2c93dede69ccc3fecec1317ee082787aa8
Jônatas da Silva Gonçalves <145167220+Jota-721@users.noreply.github.com>
2024-03-19 23:03:09 +0000

    menu-criado

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+  printf("\nLista de Tarefas\n");
+
+  int opcao;
+
+  do{
+    printf("\nMenu Principal:\n\n");
+    printf("1. Adicionar Tarefa\n");
+    printf("2. Deletar Tarefa\n");
+    printf("3. Listar Tarefas\n");
+    printf("0. Sair\n");
+
+    printf("\nDigite o número da opção desejada:\n");
+    int i = scanf("%d", &opcao);
+
+    switch(opcao){
+      case 1:
+        printf("\nAdicionar Tarefa\n");
+        break;
+      case 2:
+        printf("\nDeletar Tarefa\n");
+        break
+      case 3:
+        printf("\nListar Tarefas\n");
+        break;
+    }
+  } while(opcao != 0);
+}
