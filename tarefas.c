e8e691193dc347a6ae6510fba80de1fa1aa8ed8e
Leonardo <laferreira@fei.edu.br>
2024-03-22 22:44:14 +0000

    implementacao de criar e listar tarefas

--- tarefas.c
+++ tarefas.c
@@ -2,7 +2,20 @@
 #include <stdio.h>
 
 int criar(Tarefa tarefas[], int *pos) {
-  printf("funcao de criar tarefa\n");
+  if (*pos >= TOTAL)
+    return 1;
+
+  printf("Entre com a prioridade: ");
+  scanf("%d", &tarefas[*pos].prioridade);
+  clearBuffer();
+  printf("Entre com a categoria: ");
+  fgets(tarefas[*pos].categoria, 100, stdin);
+  clearBuffer();
+  printf("Entre com a descricao: ");
+  fgets(tarefas[*pos].descricao, 300, stdin);
+
+  *pos = *pos + 1;
+
   return 0;
 }
 int deletar(Tarefa tarefas[], int *pos) {
@@ -10,7 +23,16 @@ int deletar(Tarefa tarefas[], int *pos) {
   return 0;
 }
 int listar(Tarefa tarefas[], int pos) {
-  printf("funcao de listar tarefas\n");
+  if (pos == 0)
+    return 1;
+
+  for (int i = 0; i < pos; i++) {
+    printf("Pos: %d\t", i + 1);
+    printf("Prioridade: %d\t", tarefas[i].prioridade);
+    printf("Categoria: %s\t", tarefas[i].categoria);
+    printf("Descricao: %s\n", tarefas[i].descricao);
+  }
+
   return 0;
 }
 int salvar(Tarefa tarefas[], int total, int pos) {
@@ -20,4 +42,10 @@ int salvar(Tarefa tarefas[], int total, int pos) {
 int carregar(Tarefa tarefas[], int total, int pos) {
   printf("funcao de carregar tarefas\n");
   return 0;
+}
+
+void clearBuffer() {
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) {
+  }
 }
--- tarefas.h
+++ tarefas.h
@@ -11,3 +11,4 @@ int deletar(Tarefa tarefas[], int *pos);
 int listar(Tarefa tarefas[], int pos);
 int salvar(Tarefa tarefas[], int total, int pos);
 int carregar(Tarefa tarefas[], int total, int pos);
+void clearBuffer();
